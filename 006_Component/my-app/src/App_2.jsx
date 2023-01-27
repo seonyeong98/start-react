2453178d370f0f325413154f6e0421f33d56ac0a
DESKTOP-8N50N4L\pride <nylah@nanoomenergy.com>
2023-01-27 09:03:50 +0900

    component 만들어 따로 파일 관리

--- 006_Component/my-app/src/App_2.jsx
+++ 006_Component/my-app/src/App_2.jsx
@@ -3,7 +3,7 @@ function 개인정보() {
     <section>
       <h2>개인정보</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Nullam posuere, justo ac dignissim convallis, felis diam rhoncus arcu, congue posuere augue mauris quis metus.
+          Nullam fsdfaz posuere, justo ac dignissim convallis, felis diam rhoncus arcu, congue posuere augue mauris quis metus.
           Sed a velit eget purus vestibulum blandit nec vitae sem.
         </p>
     </section>
